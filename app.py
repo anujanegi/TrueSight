c26d1e9d0282db13b5ce0cd624f2fbd8ddc16283
unknown <ankurranjan1234@gmail.com>
2019-04-02 01:40:18 +0530

    linked stuff

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, send_from_directory
 from google.cloud import vision
+import os
+import scene_detection
+from scripts.text_to_speech import *
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="credentials.json"
 
 app = Flask(__name__)
 client = vision.ImageAnnotatorClient()
@@ -24,7 +29,11 @@ def route_detect_text():
 
 @app.route("/detect-scene")
 def route_detect_scene():
-    return ""
+    scene,obj =scene_detection.scene_detect()
+    speech(scene)
+    if obj != None:
+        speech(obj)
+    return "scene: " + scene +" obj: " + str(obj)
 
 
 @app.route("/caption")
